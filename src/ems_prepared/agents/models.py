8480b5b335d0d9d2f13a81dfad8b5e8c5487ec05
Sean Patrick Klein <sean.klein@unibw.de>
2025-08-05 16:00:53 +0200

    fixe model for system prompt

--- src/ems_prepared/agents/models.py
+++ src/ems_prepared/agents/models.py
@@ -1,7 +1,7 @@
 """Module for loading LLM models from various providers."""
 
 import os
-from dataclasses import dataclass, fields
+from dataclasses import asdict, dataclass, field, fields
 
 from openai import AsyncOpenAI
 from pydantic_ai.models.google import GoogleModel, GoogleModelSettings
@@ -16,16 +16,16 @@ from ems_prepared.state_model.emergency_call_state import EmergencyCall
 class system_prompt:
     """Represents the different parts typically found in a system prompt."""
 
-    role: str | None
-    task: str | None
-    rules: str | None
-    decisions: str | None
+    role: str | None = field(default_factory=str)
+    task: str | None = field(default_factory=str)
+    rules: str | None = field(default_factory=str)
+    decisions: str | None = field(default_factory=str)
 
     @property
     def full_prompt(self) -> str:
         """Concatenation of the parts of the prompt."""
         return "\n".join(
-            (field if isinstance(field, str) else "") for field in fields(self)
+            value for _, value in asdict(self).items() if type(value is str)
         )
 
 
